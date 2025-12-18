fccf4ad12086d699cbe6d8e26eae170cf6d845c7
anca-0 <a.traicovici@yahoo.com>
2025-12-18 22:26:52 +0200

    test

---
+++ Assets/Script/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    [SerializeField] float leftLimit;
+    [SerializeField] float rightLimit;
+    [SerializeField] float topLimit;
+    [SerializeField] float bottomLimit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
